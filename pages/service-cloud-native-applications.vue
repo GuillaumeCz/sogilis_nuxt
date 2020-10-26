660622f9668bc3c77661dd27c32dd4a8bf272b5a
GuillaumeCz <115c7a5fac@pm.me>
2020-10-26 11:35:06 +0100

    Import fith and sixth parts of cloud service page

--- pages/service-cloud-native-applications.vue
+++ pages/service-cloud-native-applications.vue
@@ -116,6 +116,73 @@
         </div>
       </div>
     </div>
+    <div class="services_zone_5 container">
+      <div class="row">
+        <div  class="d-none d-md-block col-12 col-md-6 col-lg-5 offset-lg-1">
+          <img data-aos="fade-left" data-aos-duration="2500" data-aos-delay="0" data-aos-once="true" src="/img/cloud_illustr_microservices.svg"/>
+        </div>
+
+        <div  class="d-block d-md-none col-12 col-md-6 col-lg-5 offset-lg-1">
+          <img src="/img/cloud_illustr_microservices_mobile.svg"/>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-5 offset-lg-1">
+          <h2 class="title_zone">Microservices Architecture</h2>
+          <p class="text_description">Microservices are at the heart of Cloud Native architectures. It consists in developing your application as a collection of small services. Each service implements a specific feature, runs its own process(es) and can be deployed, upgraded, scaled up/down or restarted without impacting the others. Several teams can work independently on different services, and technologie stacks can be quite different among them. This characteristic allows you to select the best tool for each problem you try to solve, and facilitates a progressive migration of an application.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="services_zone_6 container">
+      <div class="row">
+        <div class="order-2 order-md-1 col-12 col-md-6 col-lg-5 offset-lg-1">
+          <h2 class="title_zone">Yes we can.</h2>
+          <div class="col-12 col-md-10 col-lg-10">
+            <p class="text_description">At Sogilis, our experts can write your services with a wide range of technologies:</p>
+            <div class="row">
+              <div class="col-12 col-md-11 offset-md-1 col-lg-11 offset-lg-1">
+                <div class="row">
+                  <div class="col-4 col-sm-2 offset-sm-2 offset-md-0 col-md-3 col-lg-3">
+                    <img class="icone" src="/img/ico_go.jpg"/>
+                    <p class="title_name">Go</p>
+                  </div>
+                  <div class="col-4 col-sm-2 col-md-3 col-lg-3">
+                    <img class="icone" src="/img/ico_python.svg"/>
+                    <p class="title_name">Python</p>
+                  </div>
+                  <div class="col-4 col-sm-2 col-md-3 col-lg-3">
+                    <img class="icone" src="/img/ico_ruby.jpg"/>
+                    <p class="title_name">Ruby</p>
+                  </div>
+                  <div class="col-4 col-sm-2 col-md-3 col-lg-3">
+                    <img class="icone" src="/img/ico_node.svg"/>
+                    <p class="title_name">Node JS</p>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-4 col-sm-2 offset-sm-3 offset-md-0 col-md-3 col-lg-3">
+                    <img class="icone" src="/img/ico_react.svg"/>
+                    <p class="title_name">React</p>
+                  </div>
+                  <div class="col-4 col-sm-2 col-md-3 col-lg-3">
+                    <img class="icone" src="/img/ico_java.svg"/>
+                    <p class="title_name">Java</p>
+                  </div>
+                  <div class="col-4 col-sm-2 col-md-3 col-lg-3">
+                    <img class="icone" src="/img/ico_c.svg"/>
+                    <p class="title_name">C#</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="order-1 order-md-2 col-12 col-md-6 col-lg-6">
+          <img data-aos="fade-right" data-aos-duration="2500" data-aos-delay="0" data-aos-once="true" src="/img/cloud_illustr_languages.svg"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
