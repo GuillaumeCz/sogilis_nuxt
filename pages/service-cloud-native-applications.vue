1ae30c752054c70ccfeff8ac68acdb6b8124c3f9
GuillaumeCz <115c7a5fac@pm.me>
2020-10-26 11:23:14 +0100

    Import the first section of cloud service page

--- pages/service-cloud-native-applications.vue
+++ pages/service-cloud-native-applications.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>SERVICE</div>
+  <div>
+    <div class="container services_zone_1">
+      <div class="row">
+        <div class="order-2 order-md-1 col-12 col-sm-12 col-md-7 col-lg-8">
+          <img data-aos="fade-right" data-aos-duration="2500" data-aos-delay="0" data-aos-once="true" src="/img/cloud_illustr_1.svg">
+        </div>
+
+        <div class="order-1 order-md-2 col-12 col-sm-12 col-md-5 col-lg-4">
+          <img data-aos="fade-left" data-aos-duration="2500" data-aos-delay="600" data-aos-once="true" class="d-none d-md-block illustration_nuage" src="/img/illustr_nuage_native.svg">
+          <h4 class="title_subcategory"> Service Cloud Native</h4>
+          <h1 class="title_level_1">Build for the Future</h1>
+          <p class="text_montserrat">We are expert at building web applications that exploit the advantages of the cloud computing delivery model, be it private or public cloud.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
