ecb77398904afa4c4b4d61f93ec1f7b88c21dd6f
GuillaumeCz <115c7a5fac@pm.me>
2020-10-26 15:07:03 +0100

    Add language switcher

---
+++ components/Language.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="lang_selector">
+    <div v-if="isFrench">
+      <img src="/img/flag_eng.png">
+      <nuxt-link :to="switchLocalePath('en')">
+        English
+      </nuxt-link>
+    </div>
+    <div v-else>
+      <img src="/img/flag_fr.png">
+      <nuxt-link :to="switchLocalePath('fr')">
+        Français
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Language",
+  computed: {
+    isFrench() {
+      return this.$i18n.locale === 'fr';
+    }
+  }
+}
+</script>
